f80c16775a75382fa546eb8025a0af70be12b5bf
ErwanR-Holberton <6218@holbertonstudents.com>
2023-07-25 14:56:00 +0200

    Erwan T0 T1 adde hexa ocatl unsigned decimal 2nd

--- _printf.c
+++ _printf.c
@@ -1,20 +1,20 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
+
 /**
-* _printf - a copy of the printf function
-*
-* @format: strings, chars, vars
-* Return: int
-*/
-int _printf(const char *const format, ...)
+ * _printf - write a string with multiple variables
+ *
+ * @format: the list of chars & variables to print
+ *
+ * Return: the number of characters printed or failure code
+ */
+
+int _printf(const char *format, ...)
 {
-	int i = 0, j = 0, write_v = 0;
+	f_struct coresponding_char_to_funct[] = {{'c', printchar},
+	{'s', printstring}, {'d', printint}, {'i', printint}, {'u', printunsigned},
+	{'o', printoctal}, {'x', printhexa}, {'X', printhexa}};
 	va_list args;
-	ref_t array_of_funcs[] = {
-	{"c", function_char},
-	{"s", function_string},
-	};
+	int i = 0, j, w_value = 0;
 
 	va_start(args, format);
 	while (format && format[i])
@@ -23,42 +23,30 @@ int _printf(const char *const format, ...)
 		{
 			if (format[i + 1] == '%')
 			{
-				write_v = write_v + write(1, "%", 1);
+				w_value = w_value + write(1, "%", 1);
 				i++;
 			}
 			else
 			{
-				j = 0;
-
-				while (j < 2)
+				for (j = 0; j < 8 ; j++)
 				{
-					if (format[i + 1] == *array_of_funcs[j].ref)
+					if (format[i + 1] == coresponding_char_to_funct[j].func_char)
 					{
-						write_v = write_v + array_of_funcs[j].func(args);
+						w_value = w_value + coresponding_char_to_funct[j].func_to_call(args);
 						break;
 					}
-					j++;
 				}
-				if (j < 2)
-				{
+				if (j != 8)
 					i = i + 1;
-				}
 				else
-				{
-					write_v = write_v + write(1, "%", 1);
-				}
+					w_value = w_value + write(1, "%", 1);
 			}
 		}
 		else
-		{
-			write_v = write_v + write(1, &format[i], 1);
-		}
-
+			w_value = w_value + write(1, &format[i], 1);
 		i++;
 	}
-va_end(args);
-return (write_v);
+
+	va_end(args);
+	return (w_value);
 }
-
-
-
--- print_char.c
+++ print_char.c
@@ -1,17 +1,12 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
 /**
-* function_char - writes the value of c and returns count
-*
-* @args: macro of args
-* Return: int
-*/
-int function_char(va_list args)
+ * printchar - write a char
+ * @args: macro for the variadic function
+ * Return: the number of characters printed or failure code
+ */
+int printchar(va_list args)
 {
-	int count = 0;
-	int c = va_arg(args, int);
+	char c = va_arg(args, int);
 
-	count = write(1, &c, 1);
-	return (count);
+	return (write(1, &c, 1));
 }
